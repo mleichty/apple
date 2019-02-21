bb882b411701e6ee2f3f24ee792129e88bf0abe1
mleichty <marialeichty4@gmail.com>
2019-02-20 21:35:49 -0500

    Site with JSON

--- app/app.js
+++ app/app.js
@@ -2,30 +2,23 @@ function loadData() {
     //url then data that's being returned
     //can use .then which is a promise to wait until data has loaded
     $.getJSON("data/data.json", function (data) {
-        console.log(data.Sections);
-        //let is var but only available in the function that we're writing in ?
-        // for(let i = 0; i < data.Nav.length; i++) {
-        //     console.log(data.Nav[i].linkName.toUpperCase());
-        // }
+        // console.log(data.Sections);
 
         //first parameter in function is the index value of the array
-        $.each(data.Nav, function (idx, value) {
-            console.log(idx);
-            console.log(value.linkName);
+        // $.each(data.Nav, function (idx, value) {
+        //     console.log(idx);
+        //     console.log(value.navHTML);
 
-            let ln = '<a href="' + value.linkURL + '" id ='+ value.idName.toLowerCase() + '>' + value.linkName + '</a>';
-            $("nav").append(ln);
-        });
+            $(".navBar").append(data.Nav.navHTML);
+        // });
 
         $.each(data.Sections, function(idx, value) {
-            let section = '<section id="' + value.idName + '">'+value.headerName+'</section>';
-            $("#content").append(section);
+
+            $("body").append(value.sectionHTML);
         })
     });
 }
 
-//when just use function() it's an anonymous function
 $(document).ready(function(){
-    // alert("hello");
     loadData();
 });
